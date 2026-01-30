5f880d7050448cae0dae2a09af1bb015752ecb49
kazeapk-svg <kazeapk@gmail.com>
2026-01-30 12:31:25 +0800

    Update main.py

--- keybot/main.py
+++ keybot/main.py
@@ -2,8 +2,22 @@ import os
 import random
 import string
 from telegram import Update
+from threading import Thread
+from flask import Flask
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+# ===== WEBKEEP ALIVE =====
+app_web = Flask(__name__)
+OWNER_ID = int(os.getenv("OWNER_ID", "0"))
+
+@app_web.route("/")
+def home():
+    return "Bot is online!"
+
+def keep_alive():
+    port = int(os.environ.get("PORT", 10000))
+    Thread(target=lambda: app_web.run(host="0.0.0.0", port=port)).start()
+    
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 # 🔑 Key generator
@@ -26,5 +40,7 @@ def main():
     print("🤖 Bot is running using polling...")
     app.run_polling()
 
+
 if __name__ == "__main__":
+    keep_alive()
     main()
